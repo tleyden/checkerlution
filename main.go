c66ea6a0e907278b8aef4e174349d88b2f13e464
Traun Leyden <traun@couchbase.com>
2013-09-21 00:32:23 -0700

    trying to create a main..

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	ng "github.com/tleyden/neurgo"
+	"log"
+)
+
+func main() {
+
+	log.Printf("hello")
+
+	ng.SeedRandom()
+
+	// game := &Game{ourTeamId: 0}
+	// game.GameLoop()
+
+}
