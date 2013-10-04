b6f74446e498fa53c8cb2ade030b2e4db1a6f5d1
Traun Leyden <traun@couchbase.com>
2013-10-03 23:09:18 -0700

    add a checkerlution training scape

---
+++ checkerlution_scape.go
@@ -0,0 +1,31 @@
+package checkerlution
+
+import (
+	"github.com/couchbaselabs/logg"
+	cbot "github.com/tleyden/checkers-bot"
+	ng "github.com/tleyden/neurgo"
+)
+
+type CheckerlutionScape struct {
+}
+
+func NewCheckerlutionScape() *CheckerlutionScape {
+	scape := &CheckerlutionScape{}
+	return scape
+}
+
+func (scape CheckerlutionScape) Fitness(cortex *ng.Cortex) (fitness float64) {
+
+	// play a game of checkers
+	thinker := &Checkerlution{mode: TRAINING_MODE}
+	thinker.StartWithCortex(cortex, cbot.RED_TEAM)
+	game := cbot.NewGame(cbot.RED_TEAM, thinker)
+	game.GameLoop()
+
+	// get result
+	fitness = thinker.latestFitnessScore
+	logg.LogTo("MAIN", "checkerlution scape returning fitness: %v", fitness)
+
+	return
+
+}
--- main/main.go
+++ main/main.go
@@ -5,17 +5,30 @@ import (
 	"github.com/tleyden/checkerlution"
 	cbot "github.com/tleyden/checkers-bot"
 	ng "github.com/tleyden/neurgo"
+	nv "github.com/tleyden/neurvolve"
 )
+
+func init() {
+	logg.LogKeys["MAIN"] = true
+	ng.SeedRandom()
+}
 
 func main() {
 
-	logg.LogKeys["MAIN"] = true
-
-	ng.SeedRandom()
-
+	// create a checkerlution instance just to create a cortex (kludgy)
 	thinker := new(checkerlution.Checkerlution)
-	redTeam := cbot.RED_TEAM
-	game := cbot.NewGame(redTeam, thinker)
-	game.GameLoop()
+	thinker.CreateNeurgoCortex()
+	cortex := thinker.cortex
+
+	// setup the scape
+	scape := NewCheckerlutionScape()
+
+	// create a stochastic hill climber
+	shc := &nv.StochasticHillClimber{
+		FitnessThreshold:           ng.FITNESS_THRESHOLD,
+		MaxIterationsBeforeRestart: 100,
+		MaxAttempts:                10,
+	}
+	cortexTrained, succeeded := shc.TrainScape(cortex)
 
 }
