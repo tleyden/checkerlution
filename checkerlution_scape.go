6e376f5099388f85441392bc3b86d6872c960a1a
Traun Leyden <traun@couchbase.com>
2013-10-05 10:48:19 -0700

    call WaitForNextGame()

--- checkerlution_scape.go
+++ checkerlution_scape.go
@@ -18,16 +18,32 @@ func (scape *CheckerlutionScape) Fitness(cortex *ng.Cortex) (fitness float64) {
 
 	logg.LogTo("MAIN", "scape.Fitness() called, playing checkers game")
 
+	// TODO: if this is the 2nd or later round, and Fitness is called
+	// even though the residue from the previous round is still hanging
+	// around .. it will think the game is finished and return a fitness.
+	// we basically need to wait/poll until the game number changes.
+	// and we'll probably have to store the game number in the scape.
+	// OR a simpler approach - the GameLoop won't unblock until
+	// the next game is ready
+
 	// play a game of checkers
 	scape.thinker.StartWithCortex(cortex, cbot.RED_TEAM)
 	game := cbot.NewGame(cbot.RED_TEAM, scape.thinker)
 	game.GameLoop()
-	logg.LogTo("MAIN", "gameLoop finished, get fitness")
+	logg.LogTo("MAIN", "gameLoop finished")
 
-	// get result
+	// get result (TODO: this feels clunky, just call thinker.calcFitness() here)
 	fitness = scape.thinker.latestFitnessScore
-	logg.LogTo("MAIN", "checkerlution scape returning fitness: %v", fitness)
+	logg.LogTo("MAIN", "checkerlution scape fitness: %v", fitness)
 
+	// wait until the game number increments, otherwise on the
+	// next callback to this method, we'll jump into a game which
+	// is already over.
+	logg.LogTo("MAIN", "checkerlution WaitForNextGame()")
+	game.WaitForNextGame()
+	logg.LogTo("MAIN", "/ finished checkerlution WaitForNextGame()")
+
+	// re-initialize the thinker (TODO: add method for this)
 	cortex.Shutdown()
 
 	return
