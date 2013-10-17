4ade0279e41dd830086a35483915a21dc1dcd30f
Traun Leyden <traun@couchbase.com>
2013-10-16 21:09:49 -0700

    calc fitness based on average of 5 games

--- checkerlution_scape.go
+++ checkerlution_scape.go
@@ -1,9 +1,11 @@
 package checkerlution
 
 import (
+	"fmt"
 	"github.com/couchbaselabs/logg"
 	cbot "github.com/tleyden/checkers-bot"
 	ng "github.com/tleyden/neurgo"
+	"time"
 )
 
 type CheckerlutionScape struct {
@@ -18,25 +20,45 @@ func (scape *CheckerlutionScape) Fitness(cortex *ng.Cortex) (fitness float64) {
 
 	logg.LogTo("MAIN", "scape.Fitness() called, playing checkers game")
 
-	cortex.Init()
+	fitnessVals := []float64{}
 
-	// play a game of checkers
-	scape.thinker.StartWithCortex(cortex, cbot.RED_TEAM)
-	game := cbot.NewGame(cbot.RED_TEAM, scape.thinker)
-	game.GameLoop()
-	logg.LogTo("MAIN", "gameLoop finished")
+	for i := 0; i < 5; i++ {
 
-	// get result (TODO: this feels clunky, just call thinker.calcFitness() here)
-	fitness = scape.thinker.latestFitnessScore
-	logg.LogTo("MAIN", "checkerlution scape fitness: %v", fitness)
+		cortex.Init()
 
-	// wait until the game number increments, otherwise on the
-	// next callback to this method, we'll jump into a game which
-	// is already over.
-	logg.LogTo("MAIN", "checkerlution WaitForNextGame()")
-	game.WaitForNextGame()
+		// play a game of checkers
+		scape.thinker.StartWithCortex(cortex, cbot.RED_TEAM)
+		game := cbot.NewGame(cbot.RED_TEAM, scape.thinker)
+		game.GameLoop()
+		logg.LogTo("MAIN", "gameLoop finished")
 
-	cortex.Shutdown()
+		// get result (TODO: this feels clunky, just call thinker.calcFitness() here)
+		latestFitness := scape.thinker.latestFitnessScore
+		logg.LogTo("MAIN", "checkerlution scape fitness: %v", latestFitness)
+		fitnessVals = append(fitnessVals, latestFitness)
+
+		// wait until the game number increments, otherwise on the
+		// next callback to this method, we'll jump into a game which
+		// is already over.
+		logg.LogTo("MAIN", "checkerlution WaitForNextGame()")
+		game.WaitForNextGame()
+
+		cortex.Shutdown()
+
+	}
+
+	logg.LogTo("MAIN", "completed game set, calc average fitness")
+
+	total := 0.0
+	for _, fitnessVal := range fitnessVals {
+		total += fitnessVal
+	}
+	fitness = total / float64(len(fitnessVals))
+	logg.LogTo("MAIN", "checkerlution avg fitness game set: %v", fitness)
+
+	filename := fmt.Sprintf("/tmp/checkerlution-%v.json", time.Now().Unix())
+	logg.LogTo("MAIN", "Dumping cortex to %v", filename)
+	cortex.MarshalJSONToFile(filename)
 
 	return
 
