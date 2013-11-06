f371ec31e71354ec9b85934be198a19d161b49a8
Traun Leyden <traun@couchbase.com>
2013-11-06 07:53:39 -0800

    change Fitness to re-use FitnessAgainst after creating an opponent.  now, no need to run an opponent in a separate process!

--- checkerlution_scape.go
+++ checkerlution_scape.go
@@ -1,11 +1,9 @@
 package checkerlution
 
 import (
-	"fmt"
 	"github.com/couchbaselabs/logg"
 	cbot "github.com/tleyden/checkers-bot"
 	ng "github.com/tleyden/neurgo"
-	"time"
 )
 
 type CheckerlutionScape struct {
@@ -73,54 +71,12 @@ func (scape *CheckerlutionScape) FitnessAgainst(cortex *ng.Cortex, opponentCorte
 
 func (scape *CheckerlutionScape) Fitness(cortex *ng.Cortex) (fitness float64) {
 
-	logg.LogTo("DEBUG", "scape.Fitness() called, playing checkers game")
+	// create an opponent
+	thinker := &Checkerlution{}
+	thinker.CreateNeurgoCortex()
+	thinker.Cortex()
 
-	fitnessVals := []float64{}
-
-	for i := 0; i < 7; i++ {
-
-		cortex.Init()
-
-		// create a checkerlution instance
-		thinker := &Checkerlution{}
-		thinker.SetMode(TRAINING_MODE)
-		thinker.StartWithCortex(cortex, scape.team)
-
-		game := cbot.NewGame(scape.team, thinker)
-		game.SetServerUrl(scape.syncGatewayUrl)
-		game.SetFeedType(scape.feedType)
-		game.SetDelayBeforeMove(scape.randomDelayBeforeMove)
-		game.GameLoop()
-		logg.LogTo("DEBUG", "gameLoop finished")
-
-		latestFitness := thinker.latestFitnessScore
-		logg.LogTo("MAIN", "Fitness: %v", latestFitness)
-		fitnessVals = append(fitnessVals, latestFitness)
-
-		// wait until the game number increments, otherwise on the
-		// next callback to this method, we'll jump into a game which
-		// is already over.
-		logg.LogTo("MAIN", "Wait For Next Game ..")
-		game.WaitForNextGame()
-
-		cortex.Shutdown()
-
-	}
-
-	logg.LogTo("DEBUG", "Game Set Finished")
-
-	total := 0.0
-	for _, fitnessVal := range fitnessVals {
-		total += fitnessVal
-	}
-	fitness = total / float64(len(fitnessVals))
-	logg.LogTo("MAIN", "Average Fitness for Game Set: %v", fitness)
-
-	filename := fmt.Sprintf("/tmp/checkerlution-%v.json", time.Now().Unix())
-	logg.LogTo("MAIN", "Saving Cortex snapshot to %v", filename)
-	cortex.MarshalJSONToFile(filename)
-
-	return
+	return scape.FitnessAgainst(cortex, thinker.Cortex())
 
 }
 
