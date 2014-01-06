d85082d5ff0b2e181a81a8244d020831c0c4e989
Traun Leyden <traun@couchbase.com>
2014-01-05 18:14:07 -0800

    enable training mode

--- main/main.go
+++ main/main.go
@@ -12,7 +12,7 @@ import (
 func init() {
 	logg.LogKeys["MAIN"] = true
 	logg.LogKeys["DEBUG"] = true
-	logg.LogKeys["CHECKERLUTION"] = false
+	logg.LogKeys["CHECKERLUTION"] = true
 	logg.LogKeys["CHECKERLUTION_SCAPE"] = true
 	logg.LogKeys["NEURGO"] = false
 	logg.LogKeys["SENSOR_SYNC"] = false
@@ -57,7 +57,7 @@ func run() {
 }
 
 func main() {
-	MODE := 0
+	MODE := 1
 	if MODE == 0 {
 		run()
 	} else {
