8effcad582cf18ae49688cabda1b76b497ba6dfb
Traun Leyden <traun@couchbase.com>
2014-01-12 08:13:40 -0800

    tee output to out.txt

--- main/run.sh
+++ main/run.sh
@@ -1,2 +1,3 @@
 
-GOMAXPROCS=8 go run main.go -feed longpoll -syncGatewayUrl http://localhost:4984/checkers -team RED -randomDelayBeforeMove 0
+rm -f out.txt
+GOMAXPROCS=8 go run main.go -feed longpoll -syncGatewayUrl http://localhost:4984/checkers -team RED -randomDelayBeforeMove 0 2>&1 | tee out.txt
