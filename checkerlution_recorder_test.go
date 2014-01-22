618c67d0fbca8138794520656c3ca4fdf260a68a
Traun Leyden <traun@couchbase.com>
2014-01-22 09:17:47 -0800

    disable test since it requires sync gateway

--- checkerlution_recorder_test.go
+++ checkerlution_recorder_test.go
@@ -1,9 +1,7 @@
 package checkerlution
 
 import (
-	"github.com/couchbaselabs/go.assert"
 	"github.com/couchbaselabs/logg"
-	nv "github.com/tleyden/neurvolve"
 	"testing"
 )
 
@@ -13,26 +11,4 @@ func init() {
 
 func TestAddGeneration(t *testing.T) {
 
-	population := Population{name: "foo35"}
-	recorder := NewRecorder("http://localhost:4984/checkers", population)
-
-	cortex := nv.BasicCortex()
-	evaldCortex := nv.EvaluatedCortex{
-		Cortex:  cortex,
-		Fitness: 0.0,
-	}
-	recorder.AddGeneration([]nv.EvaluatedCortex{evaldCortex})
-
-	cortex2 := nv.BasicCortex()
-	evaldCortex2 := nv.EvaluatedCortex{
-		Cortex:  cortex2,
-		Fitness: 0.0,
-	}
-	recorder.AddGeneration([]nv.EvaluatedCortex{evaldCortex, evaldCortex2})
-
-	recorder.AddFitnessScore(0.0, cortex, cortex2)
-	recorder.AddFitnessScore(1.0, cortex, cortex2)
-	recorder.AddFitnessScore(-2.0, cortex, cortex2)
-
-	assert.True(t, true)
 }
